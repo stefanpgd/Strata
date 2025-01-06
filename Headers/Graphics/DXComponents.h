b2d9a190e0fd7bd1e53b85d05eb85da28a68c5a2
Stefan Pijnacker <stefanpijnacker@gmail.com>
2025-01-06 17:15:13 +0100

    Initial commit

---
+++ Headers/Graphics/DXComponents.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// A collection of common rendering components, mainly used for things like RenderingStages
+// Defaults & Helpers
+#include "Graphics/DXCommon.h"
+#include "Graphics/DXUtilities.h"
+
+// Rendering Components 
+#include "Graphics/DXCommands.h"
+#include "Graphics/DXRootSignature.h"
+#include "Graphics/DXPipeline.h"
+#include "Graphics/DXComputePipeline.h"
+#include "Graphics/Window.h"
+#include "Graphics/Texture.h"
+#include "Graphics/DXBuffer.h"
